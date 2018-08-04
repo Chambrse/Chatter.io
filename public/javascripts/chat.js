f0ebd421f71bf4bea1b1f495b6d2279f24f5bf04
Chambrse <shaneechambry@gmail.com>
2018-08-04 10:00:48 -0700

    fixed some animations

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -42,36 +42,23 @@ socket.on("chat-message", function (message) {
 
 });
 
-function moveDivs() {
-
-    console.log("movedivs");
-    $("#chatWindow").children().last().remove();
-    $(".displayDiv").css("z-index", "-=1");
-    $(".displayDiv").animate({ right: '+=25%' }, 1000);
-    $("#displayDiv").attr("id", null);
-    let newdiv = $("<div class='col-3 displayDiv animated slideInLeft' id='displayDiv' style='z-index: 4;right: 0%;'></div>");
-    $("#chatWindow").prepend(newdiv);
-
-};
-
 function changeDiv() {
     let newdiv = $("\
-<div class='col-3 displayDiv' sledID='" + (currentSledID + 3) + "' style='z-index: 4; right: 0%;'>\
+<div class='col-3 displayDiv' state='initial' sledID='" + (currentSledID + 3) + "' style='z-index: " + (currentSledID + 3) + "; right: 0%;'>\
 </div>\
-<div class='col-3 displayDiv' sledID='" + (currentSledID + 2) + "' style='z-index: 3; right: 25%;'>\
+<div class='col-3 displayDiv' state='initial'  sledID='" + (currentSledID + 2) + "' style='z-index: " + (currentSledID + 2) + "; right: 25%;'>\
 </div>\
-<div class='col-3 displayDiv' sledID='" + (currentSledID + 1) + "' style='z-index: 2; right: 50%;'>\
+<div class='col-3 displayDiv' state='initial'  sledID='" + (currentSledID + 1) + "' style='z-index: " + (currentSledID + 1) + "; right: 50%;'>\
 </div>");
     if (currentSledID % 3 === 0 && !isFirstCycle) {
-        $(".displayDiv").animate({ right: '+=75%' }, 1000);/* .then(function() {
-            $("[sledid<" + currentSledID + "]").remove();
-        }); */
+        $(".displayDiv").animate({ right: '+=75%' }, 1000);
+        $("[state=full]").animate({ opacity: '0' }, 1000,  function () {
+            $("[state=toRemove]").remove();
+        });
         $("#chatWindow").prepend(newdiv);
-        $("[sledID=" + currentSledID + "]").css("z-index", "1");
-        console.log("sledid", currentSledID);
-
     }
-    $("[sledID=" + currentSledID + "]").attr("id", "");
+    $("[sledID=" + (currentSledID - 1) + "]").attr("state", "toRemove");
+    $("[sledID=" + currentSledID + "]").attr("id", "").attr("state", "full");
     $("[sledID=" + (currentSledID + 1) + "]").attr("id", "displayDiv");
     currentSledID++;
     isFirstCycle = false;
