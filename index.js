b002234f8ea9037608487fdbaf9d80a8d3960c61
Chambrse <shaneechambry@gmail.com>
2018-08-27 23:48:56 -0700

    passport

--- index.js
+++ index.js
@@ -1,45 +1,93 @@
+
+// -----------------------
+// Imports:
+// -----------------------
+
 // Environment variables
 require("dotenv").config();
 
-// Imports
+// Server
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var path = require('path');
+var bodyParser = require('body-parser');
+var handlebars = require("express-handlebars");
+
+// Other
 let randomSentence = require("random-sentence");
+
+// Authentication
 var session = require("express-session");
-
-var db = require("./models");
+var passport = require("passport");
+var expressValidator = require("express-validator");
 // var passport = require('./config/passport.js');
+var LocalStrategy = require('passport-local').Strategy;
+var bcrypt = require('bcrypt');
 
+// -----------------------
+// Server & Socket & Database:
+// -----------------------
+
+// Models
+var db = require("./models");
 
 // Make the server
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
+// Handlebars
+app.set('views', path.join(__dirname, 'views'));
+app.engine('hbs', handlebars({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', 'hbs');
+
 // Middleware
 app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine', 'ejs');
 app.use(cookieParser());
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+Store = require('connect-session-sequelize')(session.Store);
+app.use(session({ secret: "keyboard cat", resave: false, store: new Store({ db: db.sequelize }), saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(expressValidator());
+
+// Routes
+var indexRouter = require('./routes/index_routes.js');
+app.use('/', indexRouter);
+
+passport.use(new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password'
+    }, function (username, password, done) {
+
+        db.users.findOne({ where: { email: username }, attributes: ['id', 'hash'] }).then(function (results) {
+
+            if (results.length === 0) {
+                done(null, false);
+            }
+
+            const hash = results.dataValues.hash.toString();
+
+            bcrypt.compare(password, hash, function(err, response) {
+                console.log("compare response", response);
+                if (response === true) {
+                    return done(null, {user_id: results.dataValues.id});
+                } else {
+                    return done(null, false);
+                };
+            });
+
+        });
+
+    }));
 
 // Initialize sequelize, start listening on the port.
 let port = process.env.PORT || 3000;
-db.sequelize.sync({force: true}).then(function () {
+db.sequelize.sync().then(function () {
     server.listen(port, function () {
         console.log('Server listening on port: ' + port);
     });
-});
-
-//-----------------------------------------------------------------------------
-// Routes.
-//-----------------------------------------------------------------------------
-app.get("/", function (req, res) {
-    res.render("login");
-});
-
-app.get("/chat", function (req, res) {
-    res.render("chat");
 });
 
 //-----------------------------------------------------------------------------
---
+++ routes/index_routes.js
@@ -0,0 +1,129 @@
+var express = require('express');
+var db = require("../models");
+var router = express.Router();
+var expressValidator = require("express-validator");
+var passport = require("passport");
+
+// Password Hashing
+var bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+// router.get('/ibm/:handle', function (req, res, next) {
+
+router.get("/", function (req, res) {
+    res.render("register");
+});
+
+router.get("/chat", authenticationMiddleware(), function (req, res) {
+    res.render("chat");
+});
+
+router.get("/login", function (req, res) {
+    res.render('login');
+});
+
+router.post("/login", passport.authenticate("local", {
+        successRedirect: "/chat",
+        failureRedirect: "/login"
+    }));
+
+router.get("/register", function (req, res) {
+    res.render("register");
+});
+
+router.post("/register", function (req, res) {
+
+    req.checkBody('username', 'Username field cannot be empty.').notEmpty();
+    req.checkBody('username', 'Username must be between 4-15 characters long.').len(4, 15);
+    req.checkBody('email', 'The email you entered is invalid, please try again.').isEmail();
+    req.checkBody('email', 'Email address must be between 4-100 characters long, please try again.').len(4, 100);
+    req.checkBody('password', 'Password must be between 8-100 characters long.').len(8, 100);
+    req.checkBody("password", "Password must include one lowercase character, one uppercase character, a number, and a special character.").matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.* )(?=.*[^a-zA-Z0-9]).{8,}$/, "i");
+    req.checkBody('passwordMatch', 'Password must be between 8-100 characters long.').len(8, 100);
+    req.checkBody('passwordMatch', 'Passwords do not match, please try again.').equals(req.body.password);
+
+    const errors = req.validationErrors();
+
+    if (errors) {
+        console.log(JSON.stringify(errors));
+
+        let usernameErrors = [];
+        let emailErrors = [];
+        let passwordErrors = [];
+        let passwordMatchErrors = [];
+
+        errors.forEach(function (element) {
+            switch (element.param) {
+                case "username":
+                    usernameErrors.push(element);
+                    break;
+                case "email":
+                    emailErrors.push(element);
+                    break;
+                case "password":
+                    passwordErrors.push(element);
+                    break;
+                case "passwordMatch":
+                    passwordMatchErrors.push(element);
+                default:
+            }
+        });
+
+        res.render('register', {
+            usernameErrors: usernameErrors,
+            emailErrors: emailErrors,
+            passwordErrors: passwordErrors,
+            passwordMatchErrors: passwordMatchErrors,
+            inputs: {
+                username: req.body.username,
+                email: req.body.email
+            }
+        });
+    } else {
+        bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+            if (err) throw err;
+
+            db.users.create({
+                username: req.body.username,
+                email: req.body.email,
+                hash: hash
+            }).then(function (data) {
+
+                req.login(data.dataValues.id, function (err) {
+                    res.redirect("/chat");
+                });
+
+            }).catch(function (err) {
+
+                res.render('register', {
+                    error: true,
+                    inputs: {
+                        username: req.body.username,
+                        email: req.body.email
+                    }
+                });
+
+            });
+            // res.redirect("/chat");
+        });
+    };
+});
+
+passport.serializeUser(function (user_id, done) {
+    done(null, user_id);
+});
+
+passport.deserializeUser(function (user_id, done) {
+    done(null, user_id);
+});
+
+function authenticationMiddleware() {
+    return (req, res, next) => {
+        console.log(`req.session.passport.user: ${JSON.stringify(req.session.passport)}`);
+
+        if (req.isAuthenticated()) return next();
+        res.redirect('/login')
+    }
+}
+
+module.exports = router;
